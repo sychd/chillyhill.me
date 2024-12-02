e423d6e0b0b95c687ee848443c97beebf8ed7af5
Denys Sych <denis.feelin@gmail.com>
2024-12-02 17:05:10 +0100

    added commento

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -8,10 +8,11 @@
   <title>{data.meta.title}</title>
   <meta property="og:type" content="article" />
   <meta property="og:title" content={data.meta.title} />
+  <script defer src="https://cdn.commento.io/js/commento.js"></script>
 </svelte:head>
 
-<article class="bg-gray-50 text-gray-00 bg-dots flex-grow overflow-hidden px-4 py-8">
-  <div class="container">
+<article class="bg-gray-50 text-gray-00 bg-dots flex flex-grow flex-col overflow-hidden px-4 py-8">
+  <div class="container flex flex-grow flex-col">
     <hgroup class="prose mb-6 flex flex-col items-center justify-between">
       <h1 class="mb-0 font-bold text-primary">
         {data.meta.title}
@@ -27,10 +28,14 @@
       class="prose-headings:text-gray-900
     prose
     prose-lg
+    flex-grow
     px-8
  "
     >
       {@render data.content()}
     </div>
   </div>
+  <section class="container">
+    <div id="commento"></div>
+  </section>
 </article>
